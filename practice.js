34ccb06269db8d14e6d475bc9171292fd6e1b33b
mynulislam75 <mynulislam181120@gmail.com>
2022-06-18 15:13:53 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,18 @@
+// simple variable decleration
+var myName="Mynul Islam";
+var yourName="Sabrina Marufa";
+console.log(myName)
+console.log(yourName)
+
+// simple math operation
+var firstNumber=10;
+var secondNumber=20;
+var totalNumber=firstNumber+secondNumber;
+console.log(totalNumber)
+var differenceBetween=firstNumber-secondNumber;
+console.log(differenceBetween)
+
+// math operation shorterhand
+var numberOne=10;
+numberOne=10++;
+console.log(numberOne)
