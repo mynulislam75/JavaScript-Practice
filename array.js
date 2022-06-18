f044754e5f5f7941dae86d7ec7eefa5450f81dba
mynulislam75 <mynulislam181120@gmail.com>
2022-06-18 16:14:08 +0600

    adding commit

---
+++ array.js
@@ -0,0 +1,11 @@
+// declare an array
+var arrayIs=[1,2,3,4,5,6,7,8]
+console.log(arrayIs)
+var nameAre=["Mynul","Marufa","Sabrina","Masum"]
+console.log(nameAre)
+
+// getting length
+var arrayIsLength=arrayIs.length
+var nameAreLength=nameAre.length
+console.log(arrayIsLength)
+console.log(nameAreLength)
