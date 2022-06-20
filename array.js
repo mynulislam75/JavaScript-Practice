5fc0182509ef6efde29e93cb41cc203e11839d55
mynulislam75 <mynulislam181120@gmail.com>
2022-06-20 23:41:05 +0600

    added commit

--- array.js
+++ array.js
@@ -14,3 +14,24 @@ console.log(nameAreLength)
 var nameAre=["Mynul","Marufa","Sabrina","Masum",5454]
 console.log(nameAre.indexOf(5454))
 console.log(nameAre[0])
+
+var numberList=[1,2,3,4,5,6,7,8]
+var getIndex=numberList.indexOf(5365);
+var getArrayByIndex=numberList[4]
+console.log(getArrayByIndex)
+console.log(getIndex)
+
+// ADD OR REMOVE ITEMS FROM ARRAY FROM LAST
+var nameCollection=["mynul","Marufa","Mynul","Sabrina"]
+nameCollection.pop()
+nameCollection.pop()
+nameCollection.push(5125)
+nameCollection.push("Hi")
+console.log(nameCollection)
+
+// ADD OR REMOVE ITEMS FROM ARRAY FROM LAST
+var nameCollection=["mynul","Marufa","Mynul","Sabrina"]
+nameCollection.shift()
+nameCollection.unshift(53256)
+console.log(nameCollection)
+
