0ec12a36593632520f06163c232a0e6c441574ad
mynulislam75 <mynulislam181120@gmail.com>
2022-06-20 23:50:41 +0600

    added commit

---
+++ ifElse.js
@@ -0,0 +1,15 @@
+console.log(1324)
+if(12==13){
+    console.log("its true")
+}
+
+var doIt="I will do"
+var notDo="I will not do"
+var result=3.49999
+var resultStandard=3.5
+if (result >= resultStandard){
+    console.log(doIt)
+}
+else if(result < resultStandard){
+    console.log(notDo)
+}
