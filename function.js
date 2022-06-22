f6c68f70a3cb271bae0dc093fbec869032885a7b
mynulislam75 <mynulislam181120@gmail.com>
2022-06-23 00:49:35 +0600

    adding commit

---
+++ function.js
@@ -0,0 +1,15 @@
+// Simple funtion declaration
+function myFunction(){
+    console.log(12334)
+}
+myFunction()
+
+// Passing parameter through function with return statement
+function myFunctionOne(num){
+    var numOne=10;
+    var totalNum=numOne+num;
+    return totalNum
+}
+
+var result=myFunctionOne(5550)
+console.log(result)
