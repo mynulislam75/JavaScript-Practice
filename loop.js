b566e23a49cb9bf2e6443abadacb64d19c955d62
mynulislam75 <mynulislam181120@gmail.com>
2022-06-22 19:51:42 +0600

    adding commit

---
+++ loop.js
@@ -0,0 +1,17 @@
+// WHILE LOOP
+var number=0;
+while(number<16){
+    // console.log(123455);
+    number++;
+}
+
+// for loop
+for (var i=0;i<8;i++){
+    // console.log(i)
+}
+
+// getting array index
+var arr=[1,2,3,4,5,6,78,8]
+for(var i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
