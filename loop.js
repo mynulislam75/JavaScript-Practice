c2c61e394d8f45091ce6791c5428e3531bfc4746
mynulislam75 <mynulislam181120@gmail.com>
2022-06-23 00:34:36 +0600

    added story

--- loop.js
+++ loop.js
@@ -13,5 +13,20 @@ for (var i=0;i<8;i++){
 // getting array index
 var arr=[1,2,3,4,5,6,78,8]
 for(var i=0;i<arr.length;i++){
-    console.log(arr[i])
+    // console.log(arr[i])
+}
+
+// getting more array index
+
+// Declare an array
+var myArr=["mynul","marufa","sabrina",1,2,3,4,6]
+
+// loop
+for(var i=0;i<myArr.length;i++){
+    var myArrIndex=myArr[i]
+    console.log(myArrIndex)
+    if(myArrIndex== "marufa"){
+        console.log("It's a string & value is",myArrIndex)
+    }
+ 
 }
